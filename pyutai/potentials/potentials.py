b900f50afd0c8252fe409b0892d0c5ab847f6d85
pedrobn <pedrobonillanadal@gmail.com>
2022-01-07 19:49:45 +0100

    adding a ton of documentation

--- pyutai/potentials/potentials.py
+++ pyutai/potentials/potentials.py
@@ -6,7 +6,7 @@ implemented. It is based on the impelmentations done in pgmpy.DiscreteFactor.
 Initially it will contains the classes:
 
 - Node: Abstract Base Class for Tree nodes
-- BranchNode: Class for non-terminal nodes. It contains a variable, as well as 
+- BranchNode: Class for non-terminal nodes. It contains a variable, as well as
     one child for each state of the variable.
 - LeafNode: Class for terminal nodes. It contains the associated value.
 - Tree: Wrapper class for a tree root node. It performs most of the operations.
@@ -16,7 +16,7 @@ Typical usage example:
   # data is read from a numpy ndarray object
   data = np.array(get_data())
   tree = Tree.from_array(data)
-  
+
   # We can perform most of the operations over tree. For example:
   tree.prune()
   tree.access([state_configuration])
@@ -33,7 +33,7 @@ import numpy as np
 
 
 class Node(abc.ABC):
-    """Abstract Base Class for potential tree nodes"""
+    """Abstract Base Class for potential Value-Tree nodes"""
 
     def __init__(self):
         pass
@@ -42,8 +42,8 @@ class Node(abc.ABC):
     def is_terminal(self) -> bool:
         """is_terminal returns whether a given node is terminal.
 
-        Non-terminal nodes should have a children attribute. 
-        Terminal nodes should have a value attribute. 
+        Non-terminal nodes should have a children attribute.
+        Terminal nodes should have a value attribute.
         """
 
     @abc.abstractmethod
@@ -52,16 +52,35 @@ class Node(abc.ABC):
 
 
 class BranchNode(Node):
-    """BranchNode is a node that is not terminal.
+    """BranchNode is a node is a node that has children.
 
-    A branch node is a node that has children. It is associated with
-    a variable, and has one child for every state such variable can be."""
+    A branch node is characterized by not being terminal. As we deal
+    with value-tree, each branch node has to be associated with a
+    variable, and have a children for every state possible for the
+    variable.
+
+    Attributes:
+        name (int): Name of the variable associated with the node.
+        children (List[Node]): each of the node associated with each state of variable name.
+    """
 
     def __init__(self, name: int, children: List[Node]):
+        """Initializes BranchNode
+
+        checks that the variable is a non-negative value and assign
+
+        Args:
+            name (int): Name of the variable associated with the node. It should be non-negative.
+            children (List[Node]): each of the node associated with each state of variable name.
+                It should be non-negative.
+        """
         super().__init__()
 
         if name < 0:
             raise ValueError(f'Name must be non-negative, got: {name}')
+        if not children:
+            raise ValueError(
+                f'Children must be a non-empty list, got: {children}')
 
         self.name = name
         self.children = children
@@ -75,11 +94,21 @@ class BranchNode(Node):
 
 
 class LeafNode(Node):
-    """LeafNode is a node that is terminal.
+    """LeafNode is a node that store a value.
 
-    A leaf node is a node that has no children. It only contains a value."""
+    A leaf node is a node that store a value. It has no children and thus it is not terminal.
+    It only contains a value.
+
+    Attributes:
+        value (float): value of the node.
+    """
 
     def __init__(self, value: float):
+        """Initializes LeafNode.
+
+        Args:
+            value (float): value to be stored.
+         """
         super().__init__()
         self.value = value
 
@@ -91,29 +120,21 @@ class LeafNode(Node):
         return f'{self.__class__!r}({self.value!r})'
 
 
-# Deberiamos hacer la poda directamente?
-# Usamos otra vez tail recursion. Puede ser un problema con
-# cantidades grandisimas de variables.
-def _from_array(data: np.ndarray, assigned_vars: List[int]) -> Node:
-    """TODO"""
-
-    var = len(assigned_vars)  # Next variable to be assigned
-
-    # If every variable is already selected
-    if len(data.shape) == var:
-        return LeafNode(data[tuple(assigned_vars)])
-
-    else:
-        cardinality = data.shape[var]
-        children = [
-            _from_array(data, assigned_vars + [i]) for i in range(cardinality)
-        ]
-        return BranchNode(var, children)
-
-
 @dataclasses.dataclass
 class Tree:
-    """TODO"""
+    """The summary line for a class docstring should fit on one line.
+
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. If ``napoleon_attr_annotations``
+    is True, types can be specified in the class body using ``PEP 526``
+    annotations.
+
+    Attributes:
+        attr1: Description of `attr1`.
+        attr2: Description of `attr2`.
+
+    """
 
     root: Node
     n_variables: int
@@ -123,12 +144,31 @@ class Tree:
         default_factory=collections.defaultdict, init=False)
 
     @classmethod
+    def _from_array(cls, data: np.ndarray, assigned_vars: List[int]) -> Node:
+        """Used as auxiliar for tail recursion in from_array method.
+
+        As it uses tail recursion, it may generate stack overflow for big trees."""
+        var = len(assigned_vars)  # Next variable to be assigned
+
+        # If every variable is already selected
+        if len(data.shape) == var:
+            return LeafNode(data[tuple(assigned_vars)])
+
+        else:
+            cardinality = data.shape[var]
+            children = [
+                Tree._from_array(data, assigned_vars + [i])
+                for i in range(cardinality)
+            ]
+            return BranchNode(var, children)
+
+    @classmethod
     def from_array(cls, data: np.ndarray) -> Node:
-        """TODO"""
+        """ """
         if data.size == 0:
             raise ValueError('Array should be non-empty')
 
-        return cls(root=_from_array(data, []),
+        return cls(root=Tree._from_array(data, []),
                    n_variables=len(data.shape),
                    cardinality=data.shape)
 
