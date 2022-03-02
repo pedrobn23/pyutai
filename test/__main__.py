bd0a3c5164037533d337101c57daced432f6fc40
pedrobn23 <pedrobonillanadal@gmail.com>
2022-03-02 09:58:40 +0100

    correct the main file in test folder

--- test/__main__.py
+++ test/__main__.py
@@ -12,24 +12,6 @@ def suite():
     folder = os.path.dirname(__file__)
     return unittest.TestLoader().discover(folder, pattern=pattern)
 
-
-if __name__ == '__main__':
-
-=======
-import unittest
-import sys
-
-from test import nodes_test, values_test
-
-
-def suite():
-    """Suite of test to run"""
-    node_tests = unittest.TestLoader().loadTestsFromModule(nodes_test)
-    tree_tests = unittest.TestLoader().loadTestsFromModule(values_test)
-
-    return unittest.TestSuite([node_tests, tree_tests])
-
-
 if __name__ == '__main__':
     unittest.TextTestRunner(verbosity=2).run(suite())  # Nice verbosy output
 
