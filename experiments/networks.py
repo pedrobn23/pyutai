d83c8f99c33bfed64fe2a8c6acad4ee9cc24d4fe
pedrobn23 <pedrobonillanadal@gmail.com>
2022-05-31 16:33:50 +0200

    update work

--- experiments/networks.py
+++ experiments/networks.py
@@ -7,7 +7,7 @@ from experiments import read, utils
 PATH = 'networks'
 
 
-def get(net: str):
+def _get(net: str):
     if net.endswith('.bif'):
         fullpath = os.path.join(PATH, net)
         reader = read.read(fullpath)
@@ -16,26 +16,16 @@ def get(net: str):
         raise ValueError(f'Only .bif nets are accepted, got : {net}')
 
 
-def all_():
-    counter = -1
+def all():
     for net in os.listdir(PATH):
         if net.endswith('.bif'):
-            bayesian_network = get(net)
+            bayesian_network = _get(net)
             cpds = bayesian_network.get_cpds()
-            for cpd in cpds:
-                counter += 1
-                yield (counter, cpd)
-
-
-def example(*, n_variables=6):
-    for cpd in all_():
-        if len(cpd.values.shape) == n_variables:
-            return cpd
-
+            yield (net, cpds)
 
 def medical():
     for net in ['alarm.bif', 'hepar2.bif', 'munin.bif', 'pathfinder.bif']:
-        bayesian_network = get(net)
+        bayesian_network = _get(net)
         cpds = bayesian_network.get_cpds()
         for cpd in cpds:
             yield cpd, net, bayesian_network
